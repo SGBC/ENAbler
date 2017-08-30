d5f488ce7d2dee68c0e61bcf5cc73f46b0c0c351
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-08-30 20:40:23 +0200

    added a bit of arg parsing logic

--- enabler/app.py
+++ enabler/app.py
@@ -4,5 +4,63 @@ import logging
 import argparse
 
 
+def upload(args):
+    print('Hello, world!')
+
+
 def main():
-    print("Hello, World!")
+    parser = argparse.ArgumentParser(
+        prog='enabler',
+        usage='enabler <command> [options]',
+        description='programmatic ENA submission'
+    )
+    subparsers = parser.add_subparsers(
+        title='available subcommands',
+        metavar=''
+    )
+
+    parser_upload = subparsers.add_parser(
+        'upload',
+        prog='enabler upload',
+        description='upload data to ENA',
+        help='upload data to ENA'
+    )
+    param_logging = parser_upload.add_mutually_exclusive_group()
+    param_logging.add_argument(
+        '--quiet',
+        '-q',
+        action='store_true',
+        default=False,
+        help='disable info logging. (default: %(default)s).'
+    )
+    param_logging.add_argument(
+        '--debug',
+        '-d',
+        action='store_true',
+        default=False,
+        help='enable debug logging. (default: %(default)s).'
+    )
+    parser_upload.add_argument(
+        '--input',
+        '-i',
+        metavar='',
+        help='input directory'
+    )
+    parser_upload._optionals.title = 'arguments'
+    parser_upload.set_defaults(func=upload)
+    args = parser.parse_args()
+
+    # set logger
+    try:
+        if args.quiet:
+            logging.basicConfig(level=logging.ERROR)
+        elif args.debug:
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.INFO)
+
+        args.func(args)
+    except AttributeError as e:
+        logger = logging.getLogger(__name__)
+        logger.debug(e)
+        parser.print_help()
