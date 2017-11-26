cf08ee5b825c6c62c426ded9ccbbba9717d307c3
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-11-26 14:13:20 -0300

    added submit submodule

---
+++ enabler/submit.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+
+import xml.etree.ElementTree as et
+
+
+template_dir = os.path.dirname(__file__) + '/templates/'
+
+
+def submit(args):
+    """Main function for the `enabler submit` command
+    """
+    logger = logging.getLogger(__name__)
+    logger.debug('Using verbose logger')
+    # logger.error('This function is not available yet')
+
+    write_submission_xml(args.output)
+
+
+def write_submission_xml(output_dir, action='ADD', release='RELEASE', date=''):
+    """This function writes the submission XML
+    """
+    logger = logging.getLogger(__name__)
+    submission_xml = template_dir + 'submission.xml'
+
+    action_string = '<ACTION><%s /></ACTION>' % action
+    add_action = et.fromstring(action_string)
+
+    release_string = '<ACTION><%s /></ACTION>' % release
+    add_release = et.fromstring(release_string)
+
+    tree = et.parse(submission_xml)
+    root = tree.getroot()
+
+    for actions in root.iter('ACTIONS'):
+        actions.append(add_action)
+        actions.append(add_release)
+
+    tree.write(output_dir + 'submission.xml')
