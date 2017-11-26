1410c1853c6615e4e8983c5974555ca24f418929
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-11-26 14:02:08 -0300

    adding utf-8

--- enabler/app.py
+++ enabler/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import logging
 import argparse
 
 from enabler.upload import upload
+from enabler.submit import submit
 
 
 def main():
@@ -23,15 +25,15 @@ def main():
         description='upload data to ENA',
         help='upload data to ENA'
     )
-    param_logging = parser_upload.add_mutually_exclusive_group()
-    param_logging.add_argument(
+    param_logging_u = parser_upload.add_mutually_exclusive_group()
+    param_logging_u.add_argument(
         '--quiet',
         '-q',
         action='store_true',
         default=False,
         help='disable info logging. (default: %(default)s).'
     )
-    param_logging.add_argument(
+    param_logging_u.add_argument(
         '--debug',
         '-d',
         action='store_true',
@@ -47,6 +49,44 @@ def main():
     )
     parser_upload._optionals.title = 'arguments'
     parser_upload.set_defaults(func=upload)
+
+    parser_submit = subparsers.add_parser(
+        'submit',
+        prog='enabler submit',
+        description='submit to ENA',
+        help='submit to ENA'
+    )
+    param_logging_s = parser_submit.add_mutually_exclusive_group()
+    param_logging_s.add_argument(
+        '--quiet',
+        '-q',
+        action='store_true',
+        default=False,
+        help='disable info logging. (default: %(default)s).'
+    )
+    param_logging_s.add_argument(
+        '--debug',
+        '-d',
+        action='store_true',
+        default=False,
+        help='enable debug logging. (default: %(default)s).'
+    )
+    parser_submit.add_argument(
+        '--action',
+        '-a',
+        choices=['add', 'modify', 'release', 'validate'],
+        default='add',
+        help='action to perform. (default: %(default)s).'
+    )
+    parser_submit.add_argument(
+        '--output',
+        '-o',
+        required=True,
+        help='output directory where to store the XML used for submission'
+    )
+    parser_submit._optionals.title = 'arguments'
+    parser_submit.set_defaults(func=submit)
+
     args = parser.parse_args()
 
     # set logger
